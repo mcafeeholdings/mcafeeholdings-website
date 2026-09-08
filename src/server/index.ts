5ef895c4d7ef9e85da0483eae8c77363a5000bcc
Sebastian Sereno <sebastian@serenofamily.com>
2026-09-08 10:30:51 -0500

    refactor(repo): remove development code

--- src/server/index.ts
+++
@@ -1,9 +0,0 @@
-import { createServer } from 'node:http';
-import { app } from 'server/app.ts';
-import { env } from 'server/constants/env.ts';
-
-const server = createServer(app);
-
-server.listen(env.WEBSITE_PORT, () => {
-  console.log(`Server is running on http://127.0.0.1:${env.WEBSITE_PORT}`);
-});
